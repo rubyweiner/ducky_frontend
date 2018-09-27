be36b057c3e5055e59f3e881123534fa36a0d722
Ruby Weiner <rubyweiner796@me.com>
2018-09-26 20:41:15 -0400

    search/dropdown menu

--- src/containers/HomePageContainer.js
+++ src/containers/HomePageContainer.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import {Segment, Header, Grid } from 'semantic-ui-react'
-import SearchBar from '../components/home/SearchBar'
+import {Segment, Header, Grid, Search } from 'semantic-ui-react'
 import Map from '../components/home/Map'
 import SearchResults from '../components/home/SearchResults'
 
 
 class HomePageContainer extends Component {
 
+  state = {
+    query: '',
+    profiles: [],
+    filteredProfiles: []
+  }
 
+  fetchProfiles = (query) => {
+    fetch('http://localhost:3000/profiles')
+    .then(response => response.json())
+    .then(json => this.setState({profiles: json}))
+    this.filterProfiles(query)
 
+  }
+
+  filterProfiles = (query) => {
+    let profiles = this.state.profiles
+    let filtered = profiles.filter(profile => profile.first_name.toLowerCase().includes(query))
+    this.setState({filteredProfiles: filtered})
+  }
 
   render() {
 		return (
       <Grid columns="equal">
         <Grid.Column width={5}>
           <Segment>
-            <Header as="h2">Search:</Header>
-            <SearchBar onSubmit={this.onSearch}/>
+            <div className="ui large icon input">
+              <input
+                type="text"
+                placeholder="Search"
+                value={this.state.query}
+                onChange={(event) => {
+                  this.setState({query: event.currentTarget.value})
+                  this.fetchProfiles(event.currentTarget.value.toLowerCase())
+                  }
+                }
+              />
+              <i className="search icon" />
+            </div>
           </Segment>
-          <Segment>
-            <SearchResults />
-          </Segment>
+
+          {this.state.query === '' ?
+            null
+          :
+            <SearchResults profiles={this.state.filteredProfiles}/>
+          }
+
+
         </Grid.Column>
         <Grid.Column width={5}>
           <Map />
